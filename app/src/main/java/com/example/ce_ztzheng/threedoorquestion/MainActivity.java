2e65b395e57d558b484f8189850d34cb9ee5a1b6
zzt <540435039@qq.com>
2015-10-07 09:17:21 +0800

    change2

--- app/src/main/java/com/example/ce_ztzheng/threedoorquestion/MainActivity.java
+++ app/src/main/java/com/example/ce_ztzheng/threedoorquestion/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends Activity {
      * Fragment managing the behaviors, interactions and presentation of the navigation drawer.
      */
     /**
-     * version 1.2
+     * version 1.3
      */
 
     Button bu;
